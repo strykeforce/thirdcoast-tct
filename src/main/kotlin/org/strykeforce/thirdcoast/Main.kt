89d487957a1f558e3cda5c63786b157dccc9de5b
Jeff Hutchison <jeff@jeffhutchison.com>
2019-01-09 20:19:19 -0500

    Update for 2019

--- src/main/kotlin/org/strykeforce/thirdcoast/Main.kt
+++ src/main/kotlin/org/strykeforce/thirdcoast/Main.kt
@@ -1,26 +1,12 @@
 package org.strykeforce.thirdcoast
 
-import org.koin.core.parameter.parametersOf
-import org.koin.log.Logger.SLF4JLogger
-import org.koin.standalone.KoinComponent
-import org.koin.standalone.StandAloneContext
-import org.koin.standalone.inject
-import org.strykeforce.thirdcoast.command.Command
-
-class Main : KoinComponent, Runnable {
-
-    override fun run() {
-        val toml = parseResource("/test.toml")
-        val root = Command.createFromToml(toml)
-        val shell: Shell by inject { parametersOf(root) }
-        shell.run()
-    }
-}
+import edu.wpi.first.wpilibj.RobotBase
 
 /**
- * Entry for command-line testing on laptop.
+ * Main initialization function. Do not perform any initialization here.
+ *
+ * <p>If you change your main robot class, change the parameter type.
  */
-fun main() {
-    StandAloneContext.startKoin(listOf(tctModule), logger = SLF4JLogger())
-    Main().run()
+fun main(args : Array<String>) {
+    RobotBase.startRobot(::Robot)
 }
